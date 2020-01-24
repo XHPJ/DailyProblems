979aeeb56f6e275cc2c2cc397a95f93151b6fde6
XHPJ <xheladinpopaj@outlook.de>
2020-01-24 12:25:00 +0100

    refactor problem_11.go

--- main.go
+++ main.go
@@ -20,16 +20,16 @@ func main() {
 	// 	fmt.Println(CountUivalTrees(&tree))
 
 	//Problem 9
-	numbers := []int{2, 4, 6, 2, 5}
-	numbers2 := []int{5, 1, 1, 5}
-	fmt.Println(LargestSumOfNonAdj(numbers))
-	fmt.Println(LargestSumOfNonAdj(numbers2))
+	// numbers := []int{2, 4, 6, 2, 5}
+	// numbers2 := []int{5, 1, 1, 5}
+	// fmt.Println(LargestSumOfNonAdj(numbers))
+	// fmt.Println(LargestSumOfNonAdj(numbers2))
 
 	//Problem 10
 	//	FunctionScheduler(SaySomething, 4500)
 
 	//Problem 11
-	// dictionary := []string{"dog", "deer", "deal"}
-	// predix := "de"
-	// fmt.Println(PreSelection(dictionary, predix))
+	dictionary := []string{"dog", "deer", "deal"}
+	predix := "de"
+	fmt.Println(PreSelection(dictionary, predix))
 }
